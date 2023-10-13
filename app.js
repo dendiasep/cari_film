7c09ee7135b1fcc49db52062ebbc72ce02c1a339
Asep Dendi <142903437+dendiasep@users.noreply.github.com>
2023-10-13 10:52:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //mengambil data dari API
 const getMovie = async (movieName) => {
   try {
+  
     const response = await fetch(`https://imdb8.p.rapidapi.com/auto-complete?q=${movieName}`, {
     method: 'GET',
     headers: {
@@ -22,83 +23,99 @@ const getMovie = async (movieName) => {
 //data 1: menampilkan film yang dicari
 const displayMovieName = (movieData) => {
   const movieNameDiv = document.getElementById("movieYear");
-  const movieName = movieData.d[0].l;
-  const movieRank = movieData.d[0].y;
-  
-  const element = `
-  <h2>Hasil film yang anda cari: ${movieName}, ${movieRank}<h2>
-  `;
-  movieNameDiv.innerHTML = element;
+
+  if (movieData.d.length > 0 && movieData.d[0].l) {
+    const judul = movieData.d[0].l;
+    //cetak film yang dicari
+    const element = `
+      <h2>Hasil film yang anda cari: ${judul}<h2>
+    `;
+    movieNameDiv.innerHTML = element;
+    movieNameDiv.style.color = "#005B4A"
+
+  } else {
+    movieNameDiv.innerHTML = "Film tidak ditemukan!, coba lagi.";
+    movieNameDiv.style.color = "red"
+  }
 };
+
 
 //data ke 2: menampilkan data film yang dicari
 const displayTargetMovie = (movieData) => {
   const targetMovieDIv = document.getElementById("movieInfo");
-  const targetMovie = {
-    image: movieData.d[0].i.imageUrl,
-    name : movieData.d[0].l,
-    category : movieData.d[0].qid,
-    rank : movieData.d[0].rank,
-    cast : movieData.d[0].s
-  };
-//cetak ke halaman
-  const element = `
-    <div class= "movie-container">
-      <img style = "width: 300px;"  src="${targetMovie.image}" class="movie-image">
-      <div class = "movie-info">
-        <h2>Movie Detail</h2>
-        <ul>
-          <li>Title: ${targetMovie.name}</li>
-          <li>Category : ${targetMovie.category}</li>
-          <li>Actors : ${targetMovie.cast}</li>
-          <li>Rank :${targetMovie.rank}</li>
-        </ul>
+
+  if (movieData.d[0] && movieData.d[0].i) {
+    const targetMovie = {
+      image: movieData.d[0].i.imageUrl || '', // Menyertakan nilai default jika 'imageUrl' tidak ada
+      name: movieData.d[0].l || '-',
+      category: movieData.d[0].qid || '-',
+      rank: movieData.d[0].rank || '-',
+      cast: movieData.d[0].s || '-'
+    };
+
+    const element = `
+      <div class="movie-container">
+        <img style="width: 300px;" src="${targetMovie.image}" class="movie-image">
+        <div class="movie-info">
+          <h2>Detail Film</h2>
+          <ul>
+            <li>Title: ${targetMovie.name}</li>
+            <li>Category: ${targetMovie.category}</li>
+            <li>Actors: ${targetMovie.cast}</li>
+            <li>Rank: ${targetMovie.rank}</li>
+          </ul>
+        </div>
       </div>
-    </div>
-  `;
-  targetMovieDIv.innerHTML = element;
- 
+    `;
+    targetMovieDIv.innerHTML = element;
+  } 
 };
 
+
 //data 3: menampilkan film-film yang dicari
-// const displayTargetMovies = (weatherData) => {
-//   const movies = document.getElementById("movies");
-//   showMovies = movieData.d;
+let displayTargetMovies = (movieData) => {
+  const moviesDiv = document.getElementById("movies");
+  moviesDiv.innerHTML = ''; // Bersihkan elemen "movies" sebelum menambahkan film baru
 
-//   let listOfElement = "";
+  for (let c = 0; c < movieData.d.length; c++) {
+    const movie = movieData.d[c];
+    if (movie && movie.i) {
+      const TargetMovies = {
+        image: movie.i.imageUrl || '', // Menyertakan nilai default jika 'imageUrl' tidak ada
+        name: movie.l || '-',
+        category: movie.qid || '-',
+        rank: movie.rank || '-',
+      };
 
-//   for (let z = 1; z < showMovies.lenght; z++) {
-//     const showmoviesData = {
-//       image: showMovies[z].i.imageUrl
-//     }
-//     const elements= `
-//     <div class= "movies-container">
-//     <img style = "width: 300px;"  src="${showmoviesData.image}" class="movie-image">
-//     <div class = "movie-info">
-//       <h2>Movie Detail</h2>
-//       <ul>
-//         <li>Title: </li>
-//         <li>Category: </li>
-//         <li>Rank :</li>
-//       </ul>
-//     </div>`;
-//     listOfElement += elements;
-      
-//   }
-  
-//   movies.innerHTML += listOfElement;
-
-// };
-  
-
-
-
+      moviesDiv.insertAdjacentHTML('beforeend', `
+        <div class="movies-container">
+          <img style="width: 200px;" src="${TargetMovies.image}" class="movie-image">
+          <div class="movieInfo">
+            <h2>Detail Film</h2>
+            <ul>
+              <li>Judul: ${TargetMovies.name}</li>
+              <li>Kategori: ${TargetMovies.category}</li>
+              <li>Peringkat: ${TargetMovies.rank}</li>
+            </ul>
+          </div>
+        </div>
+      `);
+    }
+  }
+};
 //fungsi button
 const searchMovie = async () => {
   // Argument ini akan digantikan dengan kata dari formulir search
   const movieName = document.getElementById("movie-name").value;
-  const movieData = await getMovie(movieName);
+  if (!movieName) {
+    return null;
+  } else {
+    let movieData = await getMovie(movieName);
   displayMovieName(movieData);
   displayTargetMovie(movieData);
-  //displayTargetMovies(movieData);
+  displayTargetMovies(movieData);
+  };
 };
+
+
+
